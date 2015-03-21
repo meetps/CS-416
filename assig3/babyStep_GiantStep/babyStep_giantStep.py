03ba63ee005d6d9c475443e73829141795e967a6
Meet Shah <meetshah1995@iitb.ac.in>
2015-03-21 23:14:11 +0530

    Added Graph Files and Graph images with the screenshot of the execution

--- assig3/babyStep_GiantStep/babyStep_giantStep.py
+++ assig3/babyStep_GiantStep/babyStep_giantStep.py
@@ -5,10 +5,13 @@
 from datetime import datetime
 import time 
 import math
-import matplotlib.pyplot as plt
 
 #Example 70 = 2**x mod 131
 
+
+#################################################################################
+# Function to Calculate the Discrete Logarithm defined here
+##################################################################################
 def calculate_discrete_log(n_i,a_i,y_i): 
     n = int(n_i)
     a = int(a_i)
@@ -43,26 +46,35 @@ def calculate_discrete_log(n_i,a_i,y_i):
     #print(((x2+1)*s - x1) % n )
     return ((x2+1)*s - x1) % n 
 
+###################################################################################
+# This ca be called here in the form mentioned below
+#print(calculate_discrete_log(131,2,70))
+###################################################################################
+
+
 prime_list=[251,1021,4051,16007,64891,251903,611839] #[8,10,12,14,16,18,20bits]
 
 prime_bit_list=[8,10,12,14,16,18,20]
 
-input_list=[70,98,86,100,63,86,130]
+input_list=[71,97,83,123,61,53,131]
 
 output_list=[]
-time_list=[]
+time_list =[[0 for x in range(7)] for x in range(7)]
 
+i=0
 for prime in prime_list:
+    j=0
     for input_no in input_list:
         startTime = time.time()
         temp_out = calculate_discrete_log(prime,2,input_no)
         time_taken= time.time() - startTime
         output_list.append(temp_out)
-        time_list.append(float(str(time_taken)[0:6]))
+        time_list[i][j] = float(str(time_taken)[0:6])
+        j=j+1
+    i=i+1
 
 print(output_list)
 print(time_list)
 
-#print(calculate_discrete_log(131,2,70))
 
 
